a49376392653ffba73a30341fd5a606f52dbfb6c
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-10-14 16:01:21 +0700

    add connection

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+// buat koneksi database
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_restapi',
+});
+
+conn.connect((err) => {
+    if(err) throw err;
+    console.log('MySql connect');
+});
+
+export default conn;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// parser application/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
