a739a8b712a7557eeeb9f14dae4ca56385515516
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-10-14 16:13:22 +0700

    setting routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// panggil routes
+let routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
