1973b26c0ffd97067595574a70d0d431435f64be
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-10-14 16:34:15 +0700

    get all data mahasiswa

--- controller.js
+++ controller.js
@@ -1,8 +1,20 @@
 'use strict';
 
 const response = require('./res');
-const connecttion = require('./koneksi');
+const connection = require('./koneksi');
+const e = require('express');
 
 exports.index = function(req, res){
-    response.ok('Apps REST API running');
+    response.ok('Apps REST API running', res);
+}
+
+// menampilkan semua data mahasiswa
+exports.getAllMahasiswa = function(req, res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
 }
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function(app) {
     let json = require('./controller');
 
     app.route('/').get(json.index);
+
+    app.route('/mahasiswa').get(json.getAllMahasiswa);
 }
